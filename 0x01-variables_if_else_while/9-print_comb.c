141d492ef67492da6d05ea60bed631cfe64951e9
Morris08 <karimimorris08@gmail.com>
2023-07-17 13:16:03 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - EPntry Point
+ *
+ * Return: Always 0 (Success)
+ */ 
+
+int main(void)
+{
+	putchar('0');
+	for (int i = 1; i <= 9; i++)
+{
+	putchar(',');
+	putchar(' ');
+	putchar(i + '0');
+}
+
+putchar('\n');
+
+return (0);
+}
