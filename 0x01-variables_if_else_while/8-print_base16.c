e755623a28f4910bb473978282244a8d072ea531
Morris08 <karimimorris08@gmail.com>
2023-07-17 13:09:32 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,14 +10,12 @@ int main(void)
 	char current_char = '0';
 
 	while (current_char <= '9')
-		putchar(current_char);
-	current_char++;
+		putchar(current_char++);
 
 	current_char = 'a';
 
 	while (current_char <= 'f')
-		putchar(current_char);
-	current_char++;
+		putchar(current_char++);
 
 putchar('\n');
 
