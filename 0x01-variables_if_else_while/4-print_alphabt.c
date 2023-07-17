8103e6d58a4a290c06a612542fbd0ec591ad2830
Morris08 <karimimorris08@gmail.com>
2023-07-17 12:15:04 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,10 @@ int main(void)
 	char current_char = 'a';
 
 	while (current_char <= 'z')
+{
 		if (current_char != 'q' && current_char != 'e')
 			putchar(current_char++);
-	putchar('n\');
-	return (0);
-}	
+}
+	putchar('\n');
+return (0);
+}
