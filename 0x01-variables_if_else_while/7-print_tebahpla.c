b25e1e4b25c2c55b4669f80114eef570f37278d1
Morris08 <karimimorris08@gmail.com>
2023-07-17 12:39:02 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char current_char = 'z';
+{
+	while (current_char <= 'a')
+		putchar(current_char++);
+}
+
+	putchar('\n');
+
+return (0);
+}
