4c2a06ff5b33a4fa3cb293373a6c985068bb7e4d
Morris08 <karimimorris08@gmail.com>
2023-07-17 11:10:45 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char current_char = 'a';
+{
+	while (current_char <= 'z')
+		putchar(current_char++);
+}
+
+        current_char = 'A';
+{
+        while (current_char <= 'Z')
+	        putchar(current_char++);
+}	
+	putchar('\n');
+
+return (0);
+}
