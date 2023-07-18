8786026c7b2cefc43930af8c948d064d7437e8f1
Morris08 <karimimorris08@gmail.com>
2023-07-18 13:29:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void print_alphabet(void) - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * print_alphabet: prints alphabets
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	while (i = 'a', i < 'z', i++)
+	_putchar(i++)
+return (0);
+}
