abd8d87bc4eceb9ad57b73e0adaab956ef36d229
Morris08 <karimimorris08@gmail.com>
2023-07-18 14:10:18 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10- Entry
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0, alpha;
+
+	while (i <= 10)
+	{
+	alpha = 'a';
+
+	while (alpha < 'z')
+	{
+	_puchar('\n');
+	alpha++;
+	}
+	_putchar('\n');
+	i++;
+	}
+}
