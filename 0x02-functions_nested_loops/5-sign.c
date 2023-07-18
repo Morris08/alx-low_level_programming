24c89c8e25e5d4bfc34edf04bc5f88cdce2e34fe
Morris08 <karimimorris08@gmail.com>
2023-07-18 16:02:56 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign- Entry point
+ *
+ * Return: 0 when n is equal -1 when n is less and 1 when n is greater
+ *
+ * @n: The character in ASCII code
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	return (1);
+	_putchar (+);
+	}
+	else if (n < 0)
+	{
+	return (-1);
+	_putchar (-);
+	}
+	else
+	{
+	return (0);
+	_putchar (0);
+	}
+}
