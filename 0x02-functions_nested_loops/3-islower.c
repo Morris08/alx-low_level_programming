2526a90cb17417e5c410ee92dace3b33e3416501
Morris08 <karimimorris08@gmail.com>
2023-07-18 14:35:07 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * @c: The character in ASCII code
+ */
+
+int _islower(int c)
+{
+	c;
+
+	if (p == 'c')
+	{
+	_puctchar(1);
+	}
+	if (p != 'c')
+	{
+	_putchar(0);
+	}
+return (0);
+}
