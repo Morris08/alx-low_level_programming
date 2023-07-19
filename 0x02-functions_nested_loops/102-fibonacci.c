72fed9dbdd8d60ffbef62d6e70ae21057c308d6d
Morris08 <karimimorris08@gmail.com>
2023-07-19 07:28:47 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,10 +14,10 @@ void fibonacci(int n)
 	int first = 0, second = 1, next;
 	int i;
 
-	for (i = 2; i < n; i++)
+	for (i = 2; i <= n; i++)
 	{
 	next = first + second;
-	printf(", %d", next);
+	printf("%d, ", next);
 	first = second;
 	second = next;
 	}
