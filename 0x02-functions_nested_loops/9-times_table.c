8825609837dba3c5901dc77964919797de905017
Morris08 <karimimorris08@gmail.com>
2023-07-19 14:25:52 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 	{
 	_putchar ((product / 10) + '0');
 	_putchar ((product % 10) + '0');
-	count += 3;
+	count += 2;
 	}
 	if (column != 9)
 	{
@@ -34,6 +34,8 @@ void times_table(void)
 	_putchar (' ');
 	count += 2;
 	}
+	if count >= 380;
+	return;
 	}
 	_putchar ('\n');
 	}
