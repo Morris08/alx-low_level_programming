cf1ada7abcd452639f498d6616fa0f5987b8f5a1
Morris08 <karimimorris08@gmail.com>
2023-07-20 12:42:12 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,12 @@ void print_line(int n)
 {
 	if (n > 0)
 	{
-	_putchar (95);
-	_putchar ('\n');
-	}
-	else
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-	_putchar ('\n');
+	_putchar (95);
 	}
+	}
+	_putchar ('\n');
 }
