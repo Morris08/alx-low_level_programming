72b1f2ec0fb423f972fb8bfbda6028d97780adad
Morris08 <karimimorris08@gmail.com>
2023-07-20 06:51:00 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c < '10'; c++)
+	_putchar (c);
+	_putchar ('\n');
+}
