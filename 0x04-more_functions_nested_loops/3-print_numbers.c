e34c7608fcf00f2c28fe09942fd42352ade3bac1
Morris08 <karimimorris08@gmail.com>
2023-07-20 08:23:28 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 {
-	char numbers[] = "0123456789\n";
 	int i;
 
-	for (i = 0; i < 11; i++
+	for (i = 0; i < 10; i++)
 	{
-	_putchar(numbers[i]);
+	_putchar(i);
 	}
+	_putchar ('\n');
 }
