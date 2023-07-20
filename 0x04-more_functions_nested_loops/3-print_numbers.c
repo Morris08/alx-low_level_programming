c5eeb60846c02135822d0ca914a399c2151127a3
Morris08 <karimimorris08@gmail.com>
2023-07-20 05:56:28 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; 1++)
+	_putchar (i);
+	_putchar ('\n');
+return (0);
+}
