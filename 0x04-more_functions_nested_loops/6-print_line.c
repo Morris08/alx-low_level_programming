d31a64956b440da17a7394438e6846fd17c12704
Morris08 <karimimorris08@gmail.com>
2023-07-20 12:15:08 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - print a line on terminal
+ *
+ * @n: number of times _ is printed
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+	_putchar (95);
+	_putchar ('\n');
+	}
+	else
+	{
+	_putchar ('\n');
+	}
+}
