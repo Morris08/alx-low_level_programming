db17d54f1977bb1c1fe7164da9de90d5775cb771
Morris08 <karimimorris08@gmail.com>
2023-07-20 07:41:52 -0700

    main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,46 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "main.h"
 /**
- * main- entry point
- * Function to determine whether a number is positive, negative, or zero.
- *
- * @param n The input number to be evaluated.
- * @return A string indicating whether the number is positive, negative, or zero.
- */
-const char* positive_or_negative(int n)
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
 {
-	if (n > 0)
-	return "positive";
-	else if (n < 0)
-	return "negative";
-	else
-	return "zero";
-}
-
-/**
- * Test function to verify the positive_or_negative() function for the case of 0.
- */
-void test_positive_or_negative_zero(void)
-{
-	int n = 0;
-	const char* expected = "zero";
-	const char* result = positive_or_negative(n);
-
-	if (strcmp(result, expected) == 0) 
-{
-	printf("Test for 0 passed. Expected: %s, Actual: %s\n", expected, result);
-}
-else
-{
-	printf("Test for 0 failed. Expected: %s, Actual: %s\n", expected, result);
-}
-}
-
-int main(void) 
-{
-	test_positive_or_negative_zero();
-
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
+
