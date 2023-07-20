5c200a3774740f80766c501887fd4608dd08d502
Morris08 <karimimorris08@gmail.com>
2023-07-20 05:11:05 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ *
+ * @c: charcters
+ *
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	if (c == i)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	}
+}
