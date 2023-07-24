2f0693ff2eb7cf5c25e4b8c39c1464aaf615cdac
Morris08 <karimimorris08@gmail.com>
2023-07-24 07:16:10 -0700

    create password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char generate_random_char()
+{
+    const char valid_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    int random_index = rand() % (sizeof(valid_chars) - 1);
+    return valid_chars[random_index];
+}
+
+void generate_password(char *password, int length)
+{
+    for (int i = 0; i < length; i++) {
+        password[i] = generate_random_char();
+    }
+    password[length] = '\0';
+}
+
+int main()
+{
+    const int password_length = 10;
+    char password[password_length + 1];
+
+
+    srand((unsigned int)time(NULL));
+
+    generate_password(password, password_length);
+    printf("Random Password: %s\n", password);
+
+    return 0;
+}
