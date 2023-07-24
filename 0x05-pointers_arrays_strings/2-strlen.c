6e798613529498a9be37694c8fd536b33cc364fe
Morris08 <karimimorris08@gmail.com>
2023-07-24 05:01:32 -0700

    size

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int_strlen - prints size
+ *
+ * @s: string
+ *
+ * return: always 0
+ */
+
+void int _strlen(char *s)
+{
+	int s;
+
+	_putchar ("%lu bytes\n", sizeof(s));
+return;
+}
