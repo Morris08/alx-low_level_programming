26241a962a0ace505f6c7eba01c58baca44bcecb
Morris08 <karimimorris08@gmail.com>
2023-07-24 01:33:25 -0700

    reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reset_to_98 - Changes the address
+ *
+ * @n: integer
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	int p = 98;
+
+	*n = &p;
+
+return (0);
+}
