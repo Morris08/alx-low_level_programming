fdbec838ab677245e84684755051c2d0beb564d9
Morris08 <karimimorris08@gmail.com>
2023-07-27 13:24:48 -0700

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void print_buffer(char *b, int size) 
+/**
+ * print_buffer - prints buffer
+ * @b: string
+ * @size: integer
+ */
+
+void print_buffer(char *b, int size)
 {
 	int i, j;
 
-	if (size <= 0) 
+	if (size <= 0)
 	{
 	printf("\n");
-	return;
+	return (0);
 	}
 
-	for (i = 0; i < size; i += 10) 
+	for (i = 0; i < size; i += 10)
 	{
 	printf("%08x ", i);
 
-	// Print hexadecimal content
-	for (j = 0; j < 10; j++) 
+
+	for (j = 0; j < 10; j++)
 	{
 	if (i + j < size)
 	printf("%02x ", (unsigned char)b[i + j]);
@@ -26,12 +32,13 @@ void print_buffer(char *b, int size)
 
 	printf(" ");
 
-	// Print ASCII content
-	for (j = 0; j < 10; j++) 
+
+	for (j = 0; j < 10; j++)
 	{
-	if (i + j < size) 
+	if (i + j < size)
 	{
 	char ch = b[i + j];
+
 	printf("%c", isprint(ch) ? ch : '.');
 	}
 	else
@@ -43,14 +50,17 @@ void print_buffer(char *b, int size)
 	printf("\n");
 	}
 }
-
-// Example usage
-int main(void) 
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
 {
 	char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
-	int size = sizeof(buffer) - 1; // Exclude the null terminator
+	int size = sizeof(buffer) - 1;
+
 	printf("%s\n", buffer);
 	printf("---------------------------------\n");
 	print_buffer(buffer, size);
-return 0;
+return (0);
 }
