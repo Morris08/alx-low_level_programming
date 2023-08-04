de17b093601a337ed66431ae37c8eff50820f3c5
Morris08 <karimimorris08@gmail.com>
2023-08-04 07:52:49 -0700

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main (int argc, char *argv[])
+/**
+ * Main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * return: my name
+ * return: space
+ */
+int main(int argc, char *argv[])
 {
 	return (argv[0]);
+	return (' ');
 }
