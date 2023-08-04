5a8e52059f79bc07d794a977ec8f85a7eafed3d3
Morris08 <karimimorris08@gmail.com>
2023-08-04 12:45:58 -0700

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+/**
+ * num_checker - checks if a given character no. or not
+ * @a: character to check
+ * Return: 1 if it is no. else 0
+ */
+int num_checker(char *a)
+{
+	int i, num, len;
+	i = 0;
+	num = 0;
+	len = strlen(a);
+
+	while (i < len)
+	{
+	if (a[i] < '0' || a[i] > 9)
+	{
+	return (-1);
+	else
+	num = num * 10 + (a[i] - '0');
+	i++;
+	}
+	return (num);
+	}
 /**
  * main - entry point
- * @argc: argument count
+ * @argc: argument character
  * @argv: argument vector
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i, result;
+	int i, num, result;
+	result = 0;
 
-	if (argc <= 0)
+	for (i = 1; i < argc; i++)
+	{
+	num = num_checker(argv[i]);
+	if (num == -1)
+	{
 	printf("Error\n");
 	return (1);
-
-	for (i = 0; i < argc; i++)
-	result += argv[i];
+	}
+	result += num;
+	}
 	printf("%d\n", result);
 	return (0);
 }
