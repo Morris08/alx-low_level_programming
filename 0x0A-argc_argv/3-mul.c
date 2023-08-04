188a51e513d411f9ae3628f77182a24f437923e6
Morris08 <karimimorris08@gmail.com>
2023-08-04 10:02:59 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, result;
+
+	if (argc < 0)
+	{
+	printf("Error \n");
+	}
+	else
+	{
+	result = i * j;
+	printf("%d\n", result);
+	}
+	return (1);
+}
