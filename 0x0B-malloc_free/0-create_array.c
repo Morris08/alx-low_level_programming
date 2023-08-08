a9c29f1dab71a243480b8562d1e7395617edb919
Morris08 <karimimorris08@gmail.com>
2023-08-08 09:39:12 -0700

    characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_array - creates arrays
+ * @size: length of array
+ * @c\: character to be printed
+ * return: Null or character
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == o)
+	{
+	return (NULL);
+	}
+	char *arr = (char *)malloc(size * sizeof(char));
+	if ( arr == NULL)
+	{
+	return (NULL);
+	}
+	for (unsigned int i = 0; i < size; i++)
+	{
+	arr[i] = c;
+	}
+	return (arr);
+}
