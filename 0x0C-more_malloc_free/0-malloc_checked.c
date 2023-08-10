52fa6d4ad77bb17d2fc8b70814a2c0dff4a7f596
Morris08 <karimimorris08@gmail.com>
2023-08-10 13:31:24 -0700

    allocate mem

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - check process
+ * @b: integer
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (p == NULL)
+	{
+	exit(98);
+	}
+	return (ptr);
+}
