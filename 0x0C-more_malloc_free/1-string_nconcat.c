9cee3bae71cde6ca081924aff876941f4490c7e5
Morris08 <karimimorris08@gmail.com>
2023-08-10 14:48:26 -0700

    merge 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,38 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *dup;
+	int i;
+	unsigned int j;
+
 	if (s1 == NULL)
 	s1 = "";
 	if (s2 == NULL)
 	s2 = "";
 
-	unsigned int len1 = 0;
+	i = 0;
 
 	while (s1[len1] != '\0')
-	len1++;
+	i++;
+	dup = malloc(sizeof(char) * (i + n + 1));
 
-	unsigned int len2 = 0;
-
-	while (s2[len2] != '\0')
-	len2++;
-
-	if (n >= len2)
-	n = len2;
-	char *result = (char *)malloc(len1 + n + 1);
-
-	if (*result == NULL)
+	if (dup == NULL)
 	{
 	return (NULL);
 	}
-	unsigned int i;
+	i = j = 0;
 
-	for (i = 0; i < len1; i++)
+	while (s1[i] != '\0')
 	{
-	result[len1 + i] = s2[i];
+	dup[i] = s1[i];
+	i++
 	}
-	result[len1 + n] = '\0';
+	while (j < n && s2[j] != '\0')
+	{
+	dup[i] = s2[j];
+	i++;
+	j++;
+	}
+	dup[i] = '\0';
 	return (result);
 }
