47d1429ea5239744a1b5336ab8f15133f5c97de5
Morris08 <karimimorris08@gmail.com>
2023-08-13 23:41:38 -0700

    integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -22,7 +22,13 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 	return (NULL);
 
-	for (i = 0; i < size && min <= max; i++; min++)
-	*(ptr + i) = min;
+	int i = 0;
+
+	while (min <= max)
+	{
+	ptr[i] = min;
+	i++;
+	min++;
+	}
 	return (ptr);
 }
