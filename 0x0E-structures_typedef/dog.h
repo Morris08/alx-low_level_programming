9348b689a90a1c91522c3c2721fd29b8fb57087d
Morris08 <karimimorris08@gmail.com>
2023-08-14 12:11:17 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * struct dog - structure for dog
+ * name: name
+ * owner: owner
+ * age: age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	return (0);
+}
