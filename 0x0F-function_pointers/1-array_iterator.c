983fb17a08534b5b40c9fde0ec5f62da7d6c0dc4
Morris08 <karimimorris08@gmail.com>
2023-08-16 08:34:16 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include 
+/**
+ * array_iterator - execute a function
+ * @array: array to be executed
+ * @size: size of array
+ * @action: call the array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+}
