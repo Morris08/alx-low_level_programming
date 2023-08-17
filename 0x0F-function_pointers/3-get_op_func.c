b254608707d99ebc8232ba07b77e95ff578e6844
Morris08 <karimimorris08@gmail.com>
2023-08-17 14:55:45 -0700

    main

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,28 @@
-#include 
+#include "3-calc.h"
+#include <string.h>
 /**
+ * get_op_func - select collect operation to perform
+ * @s: operation to perform
  */
 int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+	if (strcmp(s, ops[i].op) == 0)
+	{
+	return (ops[i].f);
+	}
+	i++;
+	}
+	return (NULL);
+}
