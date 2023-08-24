a6657e420f161c6e5b5711ef32d042b579e76763
Morris08 <karimimorris08@gmail.com>
2023-08-24 12:51:10 -0700

    list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 
 /**
  * print_list - print lists
- * @h: list to be 
+ * @h: list to be
  * Return: count
  */
 
@@ -14,12 +14,12 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-	if (h -> str == NULL)
+	if (h->str == NULL)
 	printf("[0] (nil)\n");
 	else
-	printf("[%d] %s\n", h -> len, h -> str);
+	printf("[%d] %s\n", h->len, h->str);
 	count++;
-	h = h -> next;
+	h = h->next;
 	}
 	return (count);
 }
