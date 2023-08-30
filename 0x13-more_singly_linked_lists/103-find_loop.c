6816be81c048e19d4f22e72cfdb47c56af5de99d
Morris08 <karimimorris08@gmail.com>
2023-08-30 11:57:24 -0700

    looop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * find_listint_loop - find the loop
+ * @head: pointer to head of list
+ * Return: address of the node where the loop starts, or NULL if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	h++;
+	return (node);
+}
