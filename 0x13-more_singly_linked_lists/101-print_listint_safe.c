ac21aa5da712ed4e694a65041e5a43ffc9cfbe6a
Morris08 <karimimorris08@gmail.com>
2023-08-30 11:30:21 -0700

    print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,17 +8,7 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
 
-	if (head == NULL)
-	exit(98);
-
-	while (one && two && two->next && head)
-	{
-	one = one->next;
-	two = two->next->next;
-	nodes++;
-	}
-	head = NULL;
+	head++;
 	return (nodes);
 }
