31e965f1e92effc8d7d66bbc0797f062cb72a312
Morris08 <karimimorris08@gmail.com>
2023-08-30 11:45:14 -0700

    free

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,24 +7,7 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t count = 0;
-	listint_t *current = NULL;
-	listint_t *next = NULL;
 
-	if (h = NULL || *h = NULL)
-	return (count);
-	current = *h;
-
-	while (current != NULL)
-	{
-	count++;
-	next = current->next;
-	free(current);
-	current = next;
-
-	if (current == *h)
-	break;
-	}
-
-	*h = NULL:;
+	h++;
 	return (count);
 }
