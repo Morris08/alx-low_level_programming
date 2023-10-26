7b7f73db59424a83dff7fd4cd76f401e974450bd
Morris Karimi <karimimorris08@gmail.com>
2023-10-26 13:16:34 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,32 @@
 #include "main.h"
 /**
+ *sqtRecursive - square root
+ *@n: number
+ *@m: number
+ *Return: square root
+ */
+int sqtRecursive(int n, int m)
+{
+	if (n <= 0)
+	return (-1);
+	if (n * n == m)
+	return (n);
+	return (sqtRecursive(n - 1, m));
+}
+/**
+ *_sqrt_recursion - square root
+ *@n: number
+ *Return: square root
+ *
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+	return (1);
+	return (sqtRecursive(n / 2, n));
+}
+/**
  *is_prime_number - check if number is prime
  *@n: number
  *Return: 1 or 0
@@ -7,18 +34,9 @@
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n <= 1 || _sqrt_recursion(n) >= 1)
 	return (0);
-	if (n <= 3)
+	if (_sqrt_recursion(n) == -1)
 	return (1);
-	if (n % 2 == 0 || n % 3 == 0)
-	return (0);
-	for (int i = 5; i * i <= n; i += 6)
-	{
-	if (n % i == 0 || n % (i + 2) == 0)
-	{
-	return (0);
-	}
-	}
-	return (1);
+	return (_sqrt_recursion9(n));
 }
