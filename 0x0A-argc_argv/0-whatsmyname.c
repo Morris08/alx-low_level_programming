ea900ab0f66e1d237495ab956b41af3d93eae6af
Morris Karimi <karimimorris08@gmail.com>
2023-11-01 11:38:21 -0700

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  *@argv: argument vector
  *Return: 0
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	argc = argc;
-	printf("%s\n", argv);
+	printf("%s\n", argv[0]);
+	return (0);
 }
