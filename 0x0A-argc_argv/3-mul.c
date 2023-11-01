f7f43b1f4152bf6ab5737c2d86ae51874332efd2
Morris Karimi <karimimorris08@gmail.com>
2023-11-01 12:17:03 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	int i, j, mul;
+
+	mul = i * j;
+	if (argc <= 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	else
+	printf("%d\n", mul);
+	return (0);
+}
