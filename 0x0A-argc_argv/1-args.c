d3b90b944a507bf4f805c9eb5878e4250e37d5f4
Morris Karimi <karimimorris08@gmail.com>
2023-11-01 11:52:44 -0700

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+/**
+ * main - entry
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int argc,char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc);
+	return (0);
