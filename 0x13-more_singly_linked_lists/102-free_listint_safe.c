e967bf4146bff651fb4f15949da8c7fa5b34d5f3
Morris Karimi <karimimorris08@gmail.com>
2023-11-21 09:06:36 -0800

    safe

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees list
+ * @h: pointer string
+ * Return: size of list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+
+	h++;
+	return (count);
+}
