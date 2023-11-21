40d4a11f986fb04572407292e0841b0f780da2be
Morris Karimi <karimimorris08@gmail.com>
2023-11-21 09:04:48 -0800

    safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint_safe - print linked list
+ * @head: pointer to head of list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+
+	head++;
+	return (nodes);
+}
