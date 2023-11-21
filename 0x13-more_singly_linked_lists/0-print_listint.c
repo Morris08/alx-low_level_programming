6d5c80d492d5b0cecc3e5b8cf2d437bcedada3e1
Morris Karimi <karimimorris08@gmail.com>
2023-11-21 08:44:59 -0800

    list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - prints all the elements
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t j = 0;
+
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	j++;
+	}
+	return (j);
+}
