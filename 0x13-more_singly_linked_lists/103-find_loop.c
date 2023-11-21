c6e0f0508130925dd74ee056e2af21e3767897f2
Morris Karimi <karimimorris08@gmail.com>
2023-11-21 09:16:05 -0800

    loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,6 +6,27 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	h++;
-	return (0);
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+	return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+	fast = fast->next->next;
+	slow = slow->next;
+	if (fast == slow)
+	{
+	slow = head;
+	while (slow != fast)
+	{
+	slow = slow->next;
+	fast = fast->next;
+	}
+	return (fast);
+	}
+	}
+
+	return (NULL);
 }
